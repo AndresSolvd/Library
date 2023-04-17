63eb642860545f87566da3be4c0c8a6a0d130b89
AndresSolvd <agarcianavarro.laba@solvd.com>
2023-04-17 12:24:58 -0400

    Change id values

--- src/Main.java
+++ src/Main.java
@@ -7,33 +7,33 @@ public class Main {
         System.out.println(library.getWeekDaysTimeOpen());
 
         //Create a Library Item
-        LibraryItem p1 = new LibraryItem((short)123, true, "na", "na");
+        LibraryItem p1 = new LibraryItem((short)23, true, "na", "na");
         System.out.println(p1.getAvailability());
 
         //create book
-        Book book = new Book((short)001, true, "na", "na", "1984", "George Orwell", 1949, "Secker & Warburg", "Dystopian fiction");
+        Book book = new Book((short)1, true, "na", "na", "1984", "George Orwell", 1949, "Secker & Warburg", "Dystopian fiction");
         System.out.println(book.getAuthor());
         System.out.println(book.getAvailability());
 
         //create audiobook
-        AudioBook audiobook = new AudioBook((short)002, true, "na", "na", "To Kill a Mockingbird", "Harper Lee", 1960, "J. B. Lippincott & Co", "Southern Gothic, Bildungsroman", 123456789);
+        AudioBook audiobook = new AudioBook((short)2, true, "na", "na", "To Kill a Mockingbird", "Harper Lee", 1960, "J. B. Lippincott & Co", "Southern Gothic, Bildungsroman", 123456789);
         System.out.println(audiobook.getSerialNumber());
         System.out.println(audiobook.getAvailability());
         System.out.println(audiobook.getAuthor());
 
         //create CD
-        CD cd = new CD((short)003, true, "na", "na", 112345678);
+        CD cd = new CD((short)3, true, "na", "na", 112345678);
         System.out.println(cd.getSerialNumber());
         System.out.println(cd.getAvailability());
         System.out.println(cd.getDueDate());
 
         //Create Person
-        Person person = new Person((short)004, "Oliver Atom", "932-934-2984", "oatom@email.com");
+        Person person = new Person((short)4, "Oliver Atom", "932-934-2984", "oatom@email.com");
         System.out.println(person.getName());
 
         //Create Client
 
-        Client client = new Client((short)005, "Kevin Mitnick", "849-342-0132", "kmitnick@email.com", 111111);
+        Client client = new Client((short)5, "Kevin Mitnick", "849-342-0132", "kmitnick@email.com", 111111);
         System.out.println(client.getPersonId());
         System.out.println(client.getProfessorNumber());
         System.out.println(client.getName());
@@ -42,19 +42,19 @@ public class Main {
         System.out.println(client.getLoanedBookList());
 
         //create student
-        Student student = new Student((short)006, "Fernando Vargas", "123-234-5432", "fvargas@email.com", 222222, 9999);
+        Student student = new Student((short)6, "Fernando Vargas", "123-234-5432", "fvargas@email.com", 222222, 9999);
         System.out.println(student.getStudentNumber());
         System.out.println(student.getPersonId());
         System.out.println(student.getName());
 
         //Create professor
-        Professor professor = new Professor((short)007, "Bart Simpson", "475-849-3298", "bsimpson@email.com", 333333, 8888);
+        Professor professor = new Professor((short)7, "Bart Simpson", "475-849-3298", "bsimpson@email.com", 333333, 8888);
         System.out.println(professor.getProfessorNumber());
         System.out.println(professor.getPersonId());
         System.out.println(professor.getName());
 
         //create employee
-        Employee employee = new Employee((short)008, "Vegeta Sayayin", "234-553-9813", "vegeta@email.com", "PrinceOfTheLibrary", 70000);
+        Employee employee = new Employee((short)8, "Vegeta Sayayin", "234-553-9813", "vegeta@email.com", "PrinceOfTheLibrary", (double)100000);
         System.out.println(employee.getPosition());
         System.out.println(employee.getPersonId());
         System.out.println(employee.getName());
