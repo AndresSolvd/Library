56bbb4b6cd341b6416dbc5ff60a71917328990fa
AndresSolvd <agarcianavarro.laba@solvd.com>
2023-05-07 22:45:14 -0400

    Improve UniqueWordCounter algorithm to rule out symbols

--- src/main/java/com/solvd/UniqueWordCounter.java
+++ src/main/java/com/solvd/UniqueWordCounter.java
@@ -8,15 +8,21 @@ import java.util.Arrays;
 
 public class UniqueWordCounter {
     public static void uniqueWordCounter() throws IOException {
-        // Read file, split string at one or more whitespace characters and store in array of words.
+        // Read file, remove any non alphabet character, split by whitespace characters and store in array.
         // Convert array to Stream<String>. Use distinct() to rule out repeated word, the use count() to count.
         long uniqueWordCount = Arrays.stream(
                         FileUtils.readFileToString(new File("src/main/resources/The-fixer.txt"),
-                                "UTF-8").split("\\s+"))
+                                "UTF-8").replaceAll("\\W", " ").split("\\s+"))
                 .distinct().count();
-        // Result of unique words from count() is printed in the console and in a new file called output.
+        // Print count() output on file output
         FileUtils.writeStringToFile(new File("src/main/resources/output.txt"), "Number of unique words: "
                 + uniqueWordCount, "UTF-8");
+/*
+        // Print in console
         System.out.println("Number of unique words: " + uniqueWordCount);
+        Arrays.stream(FileUtils.readFileToString(new File("src/main/resources/The-fixer.txt"),
+                                "UTF-8").replaceAll("\\W", " ").split("\\s+"))
+                .distinct().forEach(System.out::println);
+*/
     }
 }
