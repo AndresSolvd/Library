f16472fd4739a3fb2da5dfee7be2ce661d781c2f
AndresSolvd <agarcianavarro.laba@solvd.com>
2023-05-11 00:42:51 -0400

    Added method for linkedList convert in ArrayList Added Generic IOldestBookFinder, IGetBooksByGenre, and INewestBookFinder with its implementations

---
+++ src/main/java/com/solvd/interfaces/IGetBooksByGenre.java
@@ -0,0 +1,7 @@
+package com.solvd.interfaces;
+
+import com.solvd.enums.Genre;
+
+public interface IGetBooksByGenre {
+    public void iGetBooksByGenre(Genre genre);
+}
---
+++ src/main/java/com/solvd/interfaces/IOldestBookFinder.java
@@ -0,0 +1,7 @@
+package com.solvd.interfaces;
+
+import com.solvd.enums.Genre;
+
+public interface IOldestBookFinder {
+    public void iOldestBookFinder();
+}
